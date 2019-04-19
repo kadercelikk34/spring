8e6339f6fc7607f4becdf130a388f8cbf937cc24
Kader Çelik <kader.celik@etstur.com>
2019-04-19 15:23:09 +0300

    test

--- src/main/java/com/example/cotroller/StudentController.java
+++ src/main/java/com/example/cotroller/StudentController.java
@@ -1,8 +1,10 @@
 package com.example.cotroller;
 
+import com.example.contants.SpringException;
 import com.example.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,11 +17,22 @@ public class StudentController {
         return new ModelAndView("student","command", new Student());
     }
     @RequestMapping(value = "/addStudent", method = RequestMethod.POST)
-    private String addStudent(@ModelAttribute("SpringWeb")Student student, ModelMap model){
-        model.addAttribute("name", student.getName());
-        model.addAttribute("age", student.getAge());
-        model.addAttribute("id", student.getId());
+    @ExceptionHandler(value = SpringException.class)
+    public String addStudent( @ModelAttribute("HelloWeb")Student student,
+                              ModelMap model) {
+        if(student.getName().length() < 5 ){
+            throw new SpringException("Given name is too short");
+        } else {
+            model.addAttribute("name", student.getName());
+        }
 
+        if( student.getAge() < 10 ){
+            throw new SpringException("Given age is too low");
+        } else {
+            model.addAttribute("age", student.getAge());
+        }
+        model.addAttribute("id", student.getId());
         return "result";
     }
+
 }
---
+++ src/main/webapp/WEB-INF/jsp/error.jsp
@@ -0,0 +1,12 @@
+
+<html>
+<head>
+    <title>Spring Error Page</title>
+</head>
+
+<body>
+<p>An error occured, please contact webmaster.</p>
+<h3>${exception.exceptionMsg}</h3>
+
+</body>
+</html>
